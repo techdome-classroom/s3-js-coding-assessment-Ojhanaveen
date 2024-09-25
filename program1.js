23b5e5d07f37a887597ff7d055a33ecf128e1973
[Naveen Kumar] <nk9411123@gmail.com>
2024-09-25 16:01:34 +0530

    Solved both the questions,Thank you!

--- program1.js
+++ program1.js
@@ -3,8 +3,38 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+
+    if (s.length % 2 !== 0){  //this step is for checking whether the length of str is odd, if it's odd it can't be valid.
+        return false;
+    }
+    //let's store the opening brack
+
+    const Brack = [];
+
+    //lets map the closing and opening brack
+
+    const matchingBrack = {
+        ')':'(',
+        '}':'{',
+        ']':'['
+    }
+    //now we start looping
+    for(let char of s){
+        if(char === '(' || char === '{' || char === '['){
+            Brack.push(char);  // if open brack are there, pushed into stack
+        } else if(char === ')' || char ==='}' || char === ']'){ //if it's a closing brack, lets check it matchese or not!
+            if(Brack.length === 0 || Brack.pop() !== matchingBrack[char]){
+                return false;
+            }
+        }
+    }
+    return Brack.length === 0 // if stack gets empty it means all brackets were matched
 };
+
+//test-cases
+console.log(isValid("()")); 
+console.log(isValid("()[]{}"));
+console.log(isValid("(]"));
 
 module.exports = { isValid };
 
--- program2.js
+++ program2.js
@@ -3,8 +3,37 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanMap = { //Map all the roman nums to their int value.
+        'I' :1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    //looping through the str, lets check each char.
+    for(let i = 0; i < s.length; i++){
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i+1]];
+    // if the current val is less than the next, we sub it.like IV, IX, etc.
+    if (current < next) {
+        total -= current;
+    } else {
+        total += current;
+    }
+    }
+    return total; //lets see the result.
 };
+
+//test-cases
+
+console.log(romanToInt("III")); // 3
+console.log(romanToInt("IV")); // 4
+console.log(romanToInt("IX")); // 9
+console.log(romanToInt("LVIII")); // 58 
 
 
 module.exports={romanToInt}
